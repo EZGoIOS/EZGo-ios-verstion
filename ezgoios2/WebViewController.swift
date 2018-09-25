294762e764fb06cce36dd34fe6c1a50b4a04c700
vicky1000266 <vicky1000266@gmail.com>
2018-09-25 23:38:13 +0800

    Add files via upload

--- ezgoios2/WebViewController.swift
+++ ezgoios2/WebViewController.swift
@@ -8,38 +8,30 @@
 import UIKit
 import WebKit
 class WebViewController: UIViewController, WKUIDelegate {
+    
     @IBOutlet var btnMenu: UIBarButtonItem!
     var webView: WKWebView!
-    override func loadView() {
-        let webConfiguration = WKWebViewConfiguration()
-        webView = WKWebView(frame: .zero, configuration: webConfiguration)
-        webView.uiDelegate = self
-        view = webView
-    }
     
+//    override func loadView() {
+//        let webConfiguration = WKWebViewConfiguration()
+//        let fullScreenSize = UIScreen.main.bounds.size
+//        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+//        webView.uiDelegate = self
+//        self.view.addSubview(webView)
+//    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let fullScreenSize = UIScreen.main.bounds.size
         btnMenu.target = revealViewController()
         btnMenu.action = #selector(SWRevealViewController.revealToggle(_:))
         self.view.addGestureRecognizer(revealViewController().panGestureRecognizer())
-        let fullScreenSize = UIScreen.main.bounds.size
-        
+        let webConfiguration = WKWebViewConfiguration()
+        //let fullScreenSize = UIScreen.main.bounds.size
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        self.view.addSubview(webView)
         let myURL = URL(string:"https://www.apple.com")
         let myRequest = URLRequest(url: myURL!)
+        webView.frame = CGRect(x: Int((fullScreenSize.width * 0.5)) , y:Int((fullScreenSize.height * 0.07)) , width: Int((fullScreenSize.width * 0.75)) , height: Int((fullScreenSize.height * 0.18)))
         webView.load(myRequest)
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+    }}
