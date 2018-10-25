6adc0f3e592507637695396b58170a816a729e74
ＡＬ <940393@gmail.com>
2018-10-25 15:57:39 +0800

    Add files via upload

--- ezgoios2/OC/PhotoShare.m
+++ ezgoios2/OC/PhotoShare.m
@@ -8,6 +8,7 @@
 //#import <Foundation/Foundation.h>//original
 #import "PhotoShare.h"
 #import "MyDataManager.h"
+#import <Unity/PhotoManager.h>//    拍照圖片全域變數用
 #import <AVFoundation/AVFoundation.h>//相機
 #import <AssetsLibrary/AssetsLibrary.h>
 //#import <Photos/Photos.h>//相簿權限
@@ -29,9 +30,7 @@ UIImage *PHimg;
 
 @interface PhotoShare ()
 @end
-AVCaptureSession *myCaptureSession;//Oct 2
-AVCaptureStillImageOutput *myStillImageOutput;
-AVCaptureConnection *myVideoConnection = nil;
+
 UIView *pauseUnityView1;
 
 @implementation PhotoShare
@@ -43,9 +42,7 @@ UIView *pauseUnityView1;
     NSLog(@"載入PhotoShare.m");
     //==========Background Img===============
     [self.view setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"background.png"]]];
-    //建立 AVCaptureSession
-    myCaptureSession = [[AVCaptureSession alloc] init];
-    [myCaptureSession setSessionPreset:AVCaptureSessionPresetPhoto];
+    
 //
 //    FBSDKSharePhotoContent *content = [[FBSDKSharePhotoContent alloc] init]; FBSDKSharePhoto * photo = [FBSDKSharePhoto photoWithImage:img userGenerated:YES]; content.photos = @[photo]; FBSDKShareButton *shareButton = [[FBSDKShareButton alloc] init]; shareButton.shareContent = content; [FBSDKShareDialog showFromViewController:self withContent:content delegate:self];
 //    
@@ -57,23 +54,31 @@ UIView *pauseUnityView1;
 }
 
 - (IBAction)shareToFB:(id)sender {
-    // Example content. Replace with content from your app.
+    
     FBSDKSharePhoto *photo = [[FBSDKSharePhoto alloc] init];
-    
-    
-//    photo.image =PHimg;
-        photo.image = [UIImage imageNamed:@"fake"];
-    
+    photo.image =GlobImg;
+//    photo.image = [UIImage imageNamed:@"fake"];
     FBSDKSharePhotoContent *content = [[FBSDKSharePhotoContent alloc] init];
-    //            content.contentURL = [NSURL URLWithString:@"https://developers.facebook.com"];
     content.photos = @[photo];
-    
     //            FBSDKShareDialog *dialog = [[FBSDKShareDialog alloc] init];
     //            dialog.fromViewController = self;
     //            dialog.shareContent = content;
     //            dialog.mode = FBSDKShareDialogModeShareSheet;
     //            [dialog show];
-    [FBSDKShareDialog showFromViewController:self withContent:content delegate:nil];
+    //若沒有照片可分享
+    if(photo.image == nil)
+    {
+        UIAlertView *alert;
+        alert = [[UIAlertView alloc] initWithTitle:@"注意"
+                                           message:@"圖片緩存為空，請先拍照進行分享"
+                                          delegate:self
+                                 cancelButtonTitle:@"確定"
+                                 otherButtonTitles:nil];
+        [alert show];
+        
+    }else{
+        [FBSDKShareDialog showFromViewController:self withContent:content delegate:nil];
+    }
 }
 
 //返回主畫面
@@ -121,10 +126,9 @@ UIView *pauseUnityView1;
     CGFloat screenWidth = screenSize.width;
     CGFloat screenHeight = screenSize.height;
     //===============生成返回键================
-    pauseUnityView1 = [[UIView alloc] initWithFrame:CGRectMake(0, screenHeight-(screenHeight * 0.1), screenWidth, screenHeight)];//可以觸發的範圍
+    pauseUnityView1 = [[UIView alloc] initWithFrame:CGRectMake(0, screenHeight-(screenHeight * 0.1), screenWidth * 0.3, screenHeight)];//可以觸發的範圍
     //top
 //    pauseUnityView1 = [[UIView alloc] initWithFrame:CGRectMake(0, 0, screenWidth, screenHeight)];//可以觸發的範圍
-    
     UIButton *backBtnn = [[UIButton alloc] initWithFrame:
                           CGRectMake(screenWidth * 0, screenHeight * 0, screenWidth * 0.3, screenHeight * 0.1)];//x,y,寬,高
     UIButton *PicBtn = [[UIButton alloc] initWithFrame:
@@ -142,75 +146,16 @@ UIView *pauseUnityView1;
     [lbltop setBackgroundColor:[UIColor colorWithRed:(0/255.0) green:(0/255.0) blue:(0/255.0) alpha:.5]];//0.65背景色
 //    [imgV setBackgroundColor:[UIColor redColor]];//====================================================
     //add Subviews
-    [pauseUnityView1 addSubview:lbltop];
+//    [pauseUnityView1 addSubview:lbltop];
     [pauseUnityView1 addSubview:backBtnn];
-    [pauseUnityView1 addSubview:PicBtn];
-    [pauseUnityView1 addSubview:imgV];
+//    [pauseUnityView1 addSubview:PicBtn];
+//    [pauseUnityView1 addSubview:imgV];
     //pauseUnityView 是 UIView 的自訂變數名
-    
-    
-    
-    
     
     
     [[MyDataManager sharedManager].myWindow.rootViewController.view addSubview:pauseUnityView1];
     self.locationView = pauseUnityView1;//!!!!!!!
-    
-    //Oct 4
-    //建立 AVCaptureSession
-    myCaptureSession = [[AVCaptureSession alloc] init];
-    [myCaptureSession setSessionPreset:AVCaptureSessionPresetPhoto];
-    //Oct 4
-    //建立 AVCaptureDeviceInput
-    NSArray *myDevices = [AVCaptureDevice devices];
-    
-    for (AVCaptureDevice *device in myDevices) {
-        if ([device position] == AVCaptureDevicePositionBack) {
-            NSLog(@"後攝影機硬體名稱: %@", [device localizedName]);
-        }
-        
-        if ([device position] == AVCaptureDevicePositionFront) {
-            NSLog(@"前攝影機硬體名稱: %@", [device localizedName]);
-        }
-        
-        if ([device hasMediaType:AVMediaTypeAudio]) {
-            NSLog(@"麥克風硬體名稱: %@", [device localizedName]);
-        }
-    }
-    
-    //建立 AVCaptureVideoPreviewLayer
-    AVCaptureVideoPreviewLayer *myPreviewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:myCaptureSession];
-    [myPreviewLayer setVideoGravity:AVLayerVideoGravityResizeAspectFill];
-    
-    CGRect rect = CGRectMake(160, 280, 320, 240);
-    [myPreviewLayer setBounds:rect];
-    
-    UIView *myView = [[UIView alloc]initWithFrame:rect];
-    [myView.layer addSublayer:myPreviewLayer];
-    
-    [pauseUnityView1 addSubview:myView];
-    
-    NSLog(@"addSubview AVCapture");
-
-    
-    //啟用攝影機 成功
-    [myCaptureSession startRunning];
-    
- 
-//    //建立 AVCaptureStillImageOutput
-//    myStillImageOutput = [[AVCaptureStillImageOutput alloc] init];
-//    NSDictionary *myOutputSettings = [[NSDictionary alloc] initWithObjectsAndKeys:AVVideoCodecJPEG,AVVideoCodecKey,nil];
-//    [myStillImageOutput setOutputSettings:myOutputSettings];
-//    
-//    [myCaptureSession addOutput:myStillImageOutput];//!!!!!!!!!
-    
-    
 }
-
-
-
-
-
 
 
 //================返回键函数================
@@ -262,104 +207,14 @@ UIView *pauseUnityView1;
     }];
 }
 
-//========  截圖範圍  ==========
--(void)fullScreenshots{
-//    UIWindow *screenWindow = [[UIApplication sharedApplication] keyWindow];     UIGraphicsBeginImageContext(screenWindow.frame.size);//全屏截图，包括window
-//
-//    [screenWindow.layer renderInContext:UIGraphicsGetCurrentContext()];
-//    UIImage *viewImage = UIGraphicsGetImageFromCurrentImageContext();
-//    UIGraphicsEndImageContext();
-//    UIImageWriteToSavedPhotosAlbum(viewImage, nil, nil, nil);
-    NSLog(@"拍照");
-//        [self save];
-    //從 AVCaptureStillImageOutput 中取得正確類型的 AVCaptureConnection
-    for (AVCaptureConnection *connection in myStillImageOutput.connections) {
-        for (AVCaptureInputPort *port in [connection inputPorts]) {
-            if ([[port mediaType] isEqual:AVMediaTypeVideo]) {
-                
-                myVideoConnection = connection;
-                break;
-            }
-        }
-    }
-    
-    //擷取影像（包含拍照音效）
-    [myStillImageOutput captureStillImageAsynchronouslyFromConnection:myVideoConnection completionHandler:^(CMSampleBufferRef imageDataSampleBuffer, NSError *error) {
-        
-        //完成擷取時的處理常式(Block)
-        if (imageDataSampleBuffer) {
-            NSData *imageData = [AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:imageDataSampleBuffer];
-            
-            //取得的靜態影像
-            UIImage *myImage = [[UIImage alloc] initWithData:imageData];
-//            [self setImage:myImage];
-            
-            //取得影像資料（需要ImageIO.framework 與 CoreMedia.framework）
-            CFDictionaryRef myAttachments = CMGetAttachment(imageDataSampleBuffer, kCGImagePropertyExifDictionary, NULL);
-            NSLog(@"影像屬性: %@", myAttachments);
-            
-        }
-    }];
-
-//    UIImage *getImage = [self screenPhoto];
-//    UIImageView *newIM = [[UIImageView alloc] initWithImage:getImage];
-//    [newIM setFrame:CGRectMake(60, 200, 200, 700)];
-//    [self.view addSubview:newIM];
-//    [self imagePickerController];
-}
 
 //==============  存到相簿  ===============
 -(void)save{
-
-//    UIGraphicsBeginImageContextWithOptions([MyDataManager sharedManager].unityViewController.view.bounds.size, YES, [UIScreen mainScreen].scale);
-    
-     UIGraphicsBeginImageContextWithOptions([MyDataManager sharedManager].myWindow.rootViewController.view.bounds.size, NO, [UIScreen mainScreen].scale);
-    [[MyDataManager sharedManager].myWindow.rootViewController.view.layer renderInContext:UIGraphicsGetCurrentContext()];
-//    PHimg = UIGraphicsGetImageFromCurrentImageContext();
   
-    //call func
-    PHimg = [self captureScreen];
     UIGraphicsEndImageContext();
     UIImageWriteToSavedPhotosAlbum(PHimg, nil, nil, nil);  //保存到相册中
     UIAlertView * alart = [[UIAlertView alloc]initWithTitle:@"溫馨提示" message:@"相片已儲存" delegate:self cancelButtonTitle:@"確定" otherButtonTitles:nil, nil];
     [alart show];
-}
-
-
-////失敗
-//- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
-//{
-//    UIImage *chosenImage = info[UIImagePickerControllerEditedImage];
-//
-//    imgV.image = chosenImage;
-//
-//    UIImageWriteToSavedPhotosAlbum(chosenImage, nil, nil, nil);
-//
-//    [picker dismissViewControllerAnimated:YES completion:NULL];
-//}
-
-
-- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
-{
-    UIImage *image = info[UIImagePickerControllerOriginalImage];
-    
-    FBSDKSharePhoto *photo = [[FBSDKSharePhoto alloc] init];
-    photo.image = image;
-    photo.userGenerated = YES;
-    FBSDKSharePhotoContent *content = [[FBSDKSharePhotoContent alloc] init];
-    content.photos = @[photo];
-    [FBSDKShareDialog showFromViewController:self withContent:content delegate:nil];
-    
-}
-- (UIImage *) captureScreen {
-    UIWindow *keyWindow = [[UIApplication sharedApplication] keyWindow];
-    CGRect rect = [keyWindow bounds];
-    UIGraphicsBeginImageContext(rect.size);
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    [keyWindow.layer renderInContext:context];
-    UIImage *img = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    return img;
 }
 
 @end
